ef90df52b850157d7d0954e115dc35e6523e8220
YounesElBali <youneselbali71@outlook.com>
2025-03-14 18:19:03 +0100

    installed nextjs with typescript

---
+++ website-ecommerce/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <div className="w-full bg-blue-600 text-white text-center p-4 relative">
+        <p className="text-lg font-semibold">hello</p>
+      </div>
+    </div>
+  );
+}
