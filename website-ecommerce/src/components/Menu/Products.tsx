f9a097afe8e1731aa102066e109c36d4037be3fd
YounesElBali <youneselbali71@outlook.com>
2025-04-14 12:30:19 +0200

    update product image

--- website-ecommerce/src/components/Menu/Products.tsx
+++ website-ecommerce/src/components/Menu/Products.tsx
@@ -22,43 +22,45 @@ export function ProductCategories() {
   };
 
   return (
-    <div className="container mx-auto py-12 px-4">
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
-        {categories.map((category) => (
-          <div key={category.id} className="relative flex justify-center">
-            {/* Product Image */}
-            <div className="w-64 h-64 flex items-center justify-center">
-              <img
-                src={category.image}
-                alt={`${category.name} category`}
-                className="max-h-full max-w-full object-contain"
-              />
-            </div>
-            
-            {/* Category Button */}
-            <button
-              onClick={() => navigateToCategory(category.path)}
-              className="absolute bottom-0 bg-[#d6ac0a] text-black py-2 px-4 min-w-32 flex items-center justify-between font-bold"
-            >
-              <span>{category.name}</span>
-              <svg 
-                xmlns="http://www.w3.org/2000/svg" 
-                className="h-5 w-5 ml-2" 
-                fill="none" 
-                viewBox="0 0 24 24" 
-                stroke="currentColor"
-              >
-                <path 
-                  strokeLinecap="round" 
-                  strokeLinejoin="round" 
-                  strokeWidth={2} 
-                  d="M14 5l7 7m0 0l-7 7m7-7H3" 
+      <div className="container mx-auto py-12 px-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
+          {categories.map((category) => (
+            <div key={category.id} className="relative flex flex-col items-center">
+              {/* Product Image */}
+              <div className="w-50 h-50 flex items-center justify-center">
+                <img
+                  src={category.image}
+                  alt={`${category.name} category`}
+                  className="w-full h-full object-cover"
                 />
-              </svg>
-            </button>
-          </div>
-        ))}
+              </div>
+    
+              {/* Category Button */}
+              <button
+                onClick={() => navigateToCategory(category.path)}
+                className="mt-4 bg-[#d6ac0a] text-black py-2 px-4 min-w-32 flex items-center justify-between font-bold"
+              >
+                <span>{category.name}</span>
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  className="h-5 w-5 ml-2" 
+                  fill="none" 
+                  viewBox="0 0 24 24" 
+                  stroke="currentColor"
+                >
+                  <path 
+                    strokeLinecap="round" 
+                    strokeLinejoin="round" 
+                    strokeWidth={2} 
+                    d="M14 5l7 7m0 0l-7 7m7-7H3" 
+                  />
+                </svg>
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+    
+  
 }
